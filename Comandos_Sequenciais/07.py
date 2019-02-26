f6fad563aed1bf14407bf66c5814093b8013a5d4
benilton <bsa.ele@uea.edu.br>
2019-02-26 12:54:54 -0300

    Sétimo programa

---
+++ Comandos_Sequenciais/07.py
@@ -0,0 +1,11 @@
+a=input("enter with a string: ")
+b=input("enter with a string:")
+print("firs string:",a)
+print("seconde string:", b)
+
+change = a
+a=b
+b=change
+
+print("The change was a successful")
+print("firts string is", a, "and second string is",b)
