fec4064c6ec6cf64dd95be70a421c786b1cfb6c0
benilton <bsa.ele@uea.edu.br>
2019-02-26 12:53:31 -0300

    Sexto programa

---
+++ Comandos_Sequenciais/06.py
@@ -0,0 +1,16 @@
+a=int(input("Enter a number: "))
+b=int(input("Enter a number: "))
+
+print()
+print("You entered with numbers:", a,"and", b)
+
+
+change=a
+a=b
+b=change
+
+print()
+print("the changes was a successful")
+
+print("firts number: ", a)
+print("seconde number: ", b)
