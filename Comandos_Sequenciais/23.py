c0389315ba72f418d84ce235f5ec962d7cfabcb3
benilton <bsa.ele@uea.edu.br>
2019-02-28 09:24:40 -0300

    Programa de nivelamento

---
+++ Comandos_Sequenciais/23.py
@@ -0,0 +1,6 @@
+nome=input("Nome do funcionario: ")
+horas=float(input("Horas trabalhadas: "))
+valor=float(input("Valor por hora trabalhada: "))
+salario=horas*valor
+print()
+print("\n Salario", salario)
